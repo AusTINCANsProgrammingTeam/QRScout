2aaf835a95333bd9fac9d112eab6121456edcf35
NogaRoze <noga.rosenberg007@gmail.com>
2023-01-14 17:39:19 -0600

    Fixes

--- components/inputs/NumberInput.tsx
+++ components/inputs/NumberInput.tsx
@@ -19,7 +19,7 @@ export default function NumberInput(data: NumberInputProps) {
       type="number"
       min={data.min}
       max={data.max}
-      defaultValue={data.defaultValue}
+      defaultValue={}
       id={data.title}
       onChange={handleChange}
     />
