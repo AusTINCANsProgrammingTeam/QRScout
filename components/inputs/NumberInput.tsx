0b321c1c46400e25e2280384245b57d74a46cdbd
NogaRoze <noga.rosenberg007@gmail.com>
2023-01-17 17:43:36 -0600

    Fixes

--- components/inputs/NumberInput.tsx
+++ components/inputs/NumberInput.tsx
@@ -7,11 +7,7 @@ export interface NumberInputProps extends BaseInputProps {
   defaultValue?: string
 }
 
-export default function NumberInput(data: NumberInputProps) {
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    data.onChange(e.target.value)
-    
-  }
+
 
   return (
     <input
