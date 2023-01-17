06c46ccfb23f11e64a93e1ea8eb0e89187e6d9ca
NogaRoze <noga.rosenberg007@gmail.com>
2023-01-17 17:33:49 -0600

    Fixes

--- components/inputs/NumberInput.tsx
+++ components/inputs/NumberInput.tsx
@@ -4,7 +4,7 @@ import BaseInputProps from './BaseInputProps'
 export interface NumberInputProps extends BaseInputProps {
   min?: number
   max?: number
-  defaultValue?: number
+  defaultValue?: string
 }
 
 export default function NumberInput(data: NumberInputProps) {
@@ -19,7 +19,7 @@ export default function NumberInput(data: NumberInputProps) {
       type="number"
       min={data.min}
       max={data.max}
-      defaultValue={data.defaultValue}
+      defaultValue={""}
       id={data.title}
       onChange={handleChange}
     />
