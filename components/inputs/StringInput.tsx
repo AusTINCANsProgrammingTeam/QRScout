0e8111c1070f1e7fdb5e551e95dff2e899af531d
NogaRoze <noga.rosenberg007@gmail.com>
2023-01-17 19:09:58 -0600

    fix

--- components/inputs/StringInput.tsx
+++ components/inputs/StringInput.tsx
@@ -17,13 +17,13 @@ export default function StringInput(data: StringInputProps) {
 
   return (
     <textarea
-      className="focus:shadow-outline w-full capslock appearance-none break-words break-all rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+      className="focus:shadow-outline w-full  appearance-none break-words break-all rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
       disabled={data.disabled} 
       name={`${data.title}_input`}
       id={`${data.title}_input`}
       onChange={handleChange}
       defaultValue={data.defaultValue}
-      value={data.value}
+      value={data.valuetoUpperCase()}
     ></textarea>
   )
 }
