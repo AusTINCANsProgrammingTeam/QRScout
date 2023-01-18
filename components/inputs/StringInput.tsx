00c0ee066c017c12a40dee0c5c32bc1ec80cfdda
NogaRoze <noga.rosenberg007@gmail.com>
2023-01-17 18:59:21 -0600

    Uppercase fix

--- components/inputs/StringInput.tsx
+++ components/inputs/StringInput.tsx
@@ -17,7 +17,7 @@ export default function StringInput(data: StringInputProps) {
 
   return (
     <textarea
-      className="focus:shadow-outline w-full appearance-none break-words break-all rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+      className="focus:shadow-outline w-full appearance-none break-words break-all uppercase-all rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
       disabled={data.disabled}
       name={`${data.title}_input`}
       id={`${data.title}_input`}
