c6e5669fa92c3bac86a60fa203ebd31b8e1ce5b5
NogaRoze <noga.rosenberg007@gmail.com>
2023-01-17 19:35:07 -0600

    fixes

--- components/inputs/BaseInputProps.ts
+++ components/inputs/BaseInputProps.ts
@@ -27,6 +27,7 @@ export interface InputProps {
   defaultValue?: any
   min?: number
   max?: number
+  maxSize?: number
 }
 
 export type InputTypes =
--- components/inputs/StringInput.tsx
+++ components/inputs/StringInput.tsx
@@ -24,6 +24,7 @@ export default function StringInput(data: StringInputProps) {
       onChange={handleChange}
       defaultValue={data.defaultValue}
       value={data.value.toLowerCase()}
+      maxSize={data.maxSize}
     ></textarea>
   )
 }
