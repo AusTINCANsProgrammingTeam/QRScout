bd10bcbbb19b480af4f1ee0d3df7a00d6f0bfa36
jam41803 <kennysonle5.0@gmail.com>
2025-01-15 18:36:17 -0600

    Downgrade to Production release

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { ChangeEvent, useEffect, useState } from 'react'
-import configJson from '../config/2024/config.json'
+import configJson from '../config/2025/config.json'
 import {
   Config,
   InputProps,
